54cbf6e7ac8ecfdab6b957a51a0e9f64361cd67b
sam <s.gelernter@gmail.com>
2021-10-13 10:52:41 -0400

    complete human_player class

--- tic_tac_toe_v1/human_player.rb
+++ tic_tac_toe_v1/human_player.rb
@@ -1,3 +1,24 @@
+require "byebug"
+
 class HumanPlayer
-    
+    def initialize(mark_value)
+        @mark = mark_value
+    end
+
+    attr_reader :mark
+
+    def get_position
+        puts "Enter coordinates to place a #{mark} on the grid, using two numbers separated by a comma:"
+        entry = gets.chomp
+        position = []
+        entry.split(",").each { |num| position << num }
+        raise "Please follow instructions" if position.length != 2 || position.any? { |ele| ele.to_i.to_s != ele.strip }
+        position.map {|num| num.to_i }
+    end
+
 end
+
+roland = HumanPlayer.new(:>)
+p roland.mark
+puts 
+p roland.get_position
