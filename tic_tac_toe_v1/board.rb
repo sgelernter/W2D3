8c6ee78b8762f0fd2458695d6db37fb57656373a
sam <s.gelernter@gmail.com>
2021-10-13 10:17:11 -0400

    complete board class

--- tic_tac_toe_v1/board.rb
+++ tic_tac_toe_v1/board.rb
@@ -44,7 +44,51 @@ class Board
         # why does this throw an error when using "print" rather than "p" or "puts"? 
     end
 
+    def win_row?(mark)
+        grid.each { |row| return true if row.all?(mark) }
+        false
+    end
+
+    def win_col?(mark)
+        grid.transpose.each { |col| return true if col.all?(mark) }
+        false
+    end
+
+    def win_diagonal?(mark)
+        diag_leftright = []
+        diag_rightleft = []
+        (0..2).each do |row|
+            (0..2).each do |col|
+                diag_leftright << grid[row][col] if row == col
+                diag_rightleft << grid[row][col] if row + col == 2
+            end
+        end
+        diag_leftright.all?(mark) || diag_rightleft.all?(mark)
+    end
+
+    def win?(mark)
+        return true if win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+        false
+    end
+
+    def empty_positions?
+        grid.each { |row| return true if row.any?("_") }
+        false
+    end
 end
 
-x = Board.new
-x.print
+# x = Board.new               #initialize
+# pos = [1, 0]          
+# pos2 = [1, 1]
+# pos3 = [1, 2] 
+# pos4 = [0, 1]
+# pos5 = [2, 1]
+# pos6 = [0, 2]
+# pos7 = [2, 0]
+# x.place_mark(pos2, :X)
+# x.place_mark(pos6, :X)
+# x.place_mark(pos7, :X)
+# x.print                     #print grid
+# p x.win_row?(:X)
+# p x.win_col?(:X)
+# p x.win_diagonal?(:X)
